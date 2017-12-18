b9065b2b917e4d2483acfa30fb73af9c042f564c
Jesse Tomchak <jtomchak@gmail.com>
2017-12-18 16:19:58 -0700

    list rendered our

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("React Away!!!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const titles = ["cookies are good", "Star Wars is ok", "Eggnog is amazing"];
+
+var MyTitle = props => {
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
+  );
+};
+
+const MyFirstComponent = () => {
+  const listItems = titles.map(title => (
+    <li style={{ color: "peru" }}>{title}</li>
+  ));
+  return <ul>{listItems}</ul>;
+};
+
+ReactDOM.render(<MyFirstComponent />, document.getElementById("root"));
